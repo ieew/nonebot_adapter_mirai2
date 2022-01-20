f3255597a607124b447c94a118757bbd39ce2476
xxxx <xxx@xxxx.com>
2022-01-20 14:15:53 +0800

    调整

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='nonebot_adapter_mirai2',
+    python_requires='>=3.8.0',
+    version='2.0.0b1',
+    description='nonebot2.0.0b1 的 mirai_api_http2 适配器',
+    author='ieew',
+    author_email='i@ieew.cc',
+    url='https://github.com/ieew/nonebot2_adapter_mirai',
+    packages=['nonebot', 'nonebot.adapters.mirai2', 'nonebot.adapters.mirai2.event'],
+    install_requires=['nonebot2>=2.0.0b1'],
+    license='GNU AFFERO GENERAL PUBLIC LICENSE',
+    keywords=['nonebot2', 'mirai', 'mirai_api_http2']
+)
