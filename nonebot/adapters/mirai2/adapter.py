077882d5fd9cd617d1a1f623c5b1c3e876162491
ieew <44091335+ieew@users.noreply.github.com>
2022-02-24 12:35:49 +0800

    Revert "update adapter"

--- nonebot/adapters/mirai2/adapter.py
+++ nonebot/adapters/mirai2/adapter.py
@@ -91,14 +91,12 @@ class Adapter(BaseAdapter):
                         log.warn(f'{json_data["data"]["msg"]}: {self_id}')
                         return
                     self.connections[str(self_id)] = ws
-                    
-                    bot = Bot(self, str(self_id))
-                    self.bot_connect(bot)
 
                     try:
                         while True:
                             data = await ws.receive()
                             json_data = json.loads(data)
+                            bot = Bot(self, str(self_id))
                             if int(json_data.get('syncId') or '0') >= 0:
                                 SyncIDStore.add_response(json_data)
                                 continue
