dd16c8109a2c00c597dbea49be90ad388797139b
ieew <44091335+ieew@users.noreply.github.com>
2022-12-08 16:09:07 +0800

    Revert "修复不能正确处理@消息的问题"

--- nonebot/adapters/mirai2/utils.py
+++ nonebot/adapters/mirai2/utils.py
@@ -34,13 +34,12 @@ def process_quote(bot: "Bot", event: Union[MessageEvent, GroupMessage]) -> Messa
 
 
 def process_at(bot: "Bot", event: GroupMessage) -> GroupMessage:
-    count = 0
-    for msg in event.message_chain:
-        if (msg.type == MessageType.AT) and (msg.data.get('target', '') == event.self_id):
+    at = event.message_chain.extract_first(MessageType.AT)
+    if at is not None:
+        if at.data['target'] == event.self_id:
             event.to_me = True
-            event.message_chain.pop(count)
-            break
-        count += 1
+        else:
+            event.message_chain.insert(0, at)
     if not event.message_chain:
         event.message_chain.append(MessageSegment.plain(''))
     return event
